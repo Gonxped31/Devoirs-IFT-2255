5ee00e16207bf0dac3745305a7855b05de37b750
Boobsone <101299691+Boobsone@users.noreply.github.com>
2023-05-28 17:14:05 -0400

    ajout de l'architecture et prototype fournisseur

---
+++ robotix/src/domain/logic/Robot/Robot.java
@@ -0,0 +1,12 @@
+package domain.logic.Robot;
+
+import java.util.UUID;
+
+public class Robot implements RobotInterface{
+    private String nom;
+    private UUID numeroDeSerie;
+    private TypeRobot typeRobot;
+    public Robot(String nom, UUID numeroDeSerie,TypeRobot typeRobot) {
+    //TODO
+    }
+}
---
+++ robotix/src/domain/logic/Robot/RobotInterface.java
@@ -0,0 +1,10 @@
+package domain.logic.Robot;
+
+import java.util.UUID;
+
+public interface RobotInterface {
+    String nom=null;
+    UUID numeroDeSerie = null;
+    enum  TypeRobot{};
+
+}
