148b835d60f627088590c3851bc411755965b6c0
gonxped31 <gonxped31@gmail.com>
2023-05-28 21:37:23 -0400

    Test

--- robotix/ClasseUtilisateur/Utilisateurs.java
+++ robotix/ClasseUtilisateur/Utilisateurs.java
@@ -2,8 +2,4 @@ public class Utilisateurs {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-
-    public static void name() {
-        System.out.print("Nothing")
-    }
 }
