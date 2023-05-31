2f08dcfe0c96d7841fb233737b8825064079f46e
gonxped31 <gonxped31@gmail.com>
2023-05-30 23:01:57 -0400

    Nothing

--- robotix/src/domain/logic/Fournisseur/Fournisseur.java
+++ robotix/src/domain/logic/Fournisseur/Fournisseur.java
@@ -6,14 +6,14 @@ import domain.logic.Robot.TypeRobot;
 import java.util.List;
 
 public class Fournisseur{
-    private String nom;
-    private String adresse;
-    private String email;
-    private String numeroTelephone;
-    private TypeRobot typeRobotFabriquer;
-    private double capacite;
-    private String nomcompagnie;
-    private List<Robot> inventaireDeRobot;
+    String nom;
+    String adresse;
+    String email;
+    String numeroTelephone;
+    TypeRobot typeRobotFabriquer;
+    double capacite;
+    String nomcompagnie;
+    List<Robot> inventaireDeRobot;
 
     private List<Composant> composants;
     public Fournisseur(String nom, String adresse, String email, String numeroTelephone,TypeRobot typeDeRobotFabriquer, double capacite){
