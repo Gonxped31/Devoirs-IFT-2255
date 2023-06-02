ebddce37605597770da922edcd63c236f2a59e14
Boobsone <101299691+Boobsone@users.noreply.github.com>
2023-06-02 11:13:00 -0400

    implementation des methodes de la class fournisseur

--- robotix/src/domain/logic/Fournisseur/Fournisseur.java
+++ robotix/src/domain/logic/Fournisseur/Fournisseur.java
@@ -3,6 +3,7 @@ package domain.logic.Fournisseur;
 import domain.logic.Robot.Robot;
 import domain.logic.Robot.TypeRobot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Fournisseur{
@@ -12,14 +13,38 @@ public class Fournisseur{
     String numeroTelephone;
     TypeRobot typeRobotFabriquer;
     double capacite;
-    String nomcompagnie;
-    List<Robot> inventaireDeRobot;
+    String nomCompagnie;
+    String pseudo;
+    List<Robot> inventaireDeRobot=new ArrayList<>();
+    List<Composant> inventaireComposant= new ArrayList<>();
 
-    private List<Composant> composants;
-    public Fournisseur(String nom, String adresse, String email, String numeroTelephone,TypeRobot typeDeRobotFabriquer, double capacite){
-        //TODO
+    public Fournisseur(String nom, String adresse, String pseudo, String email, String numeroTelephone,
+                       TypeRobot typeDeRobotFabriquer, double capacite ,String nomcompagnie){
+        this.nom=nom;
+        this.adresse=adresse;
+        this.pseudo=pseudo;
+        this.email=email;
+        this.numeroTelephone=numeroTelephone;
+        this.typeRobotFabriquer=typeDeRobotFabriquer;
+        this.capacite=capacite;
+        this.nomCompagnie=nomcompagnie;
     }
-    public void vendreUnComposant(){
-        //TODO
+    public void vendreUnComposant(Composant composant){
+      inventaireComposant.remove(composant);
+    }
+    public void mettreInventaireRobotAjour(Robot robot, boolean modeAjout){
+        if (modeAjout) {
+            inventaireDeRobot.add(robot);
+        } else {
+            inventaireDeRobot.remove(robot);
+        }
+    }
+
+    public void mettreInventaireComposantAjour( Composant composant, boolean modeAjout){
+        if (modeAjout) {
+            inventaireComposant.add(composant);
+        } else {
+            inventaireComposant.remove(composant);
+        }
     }
 }
