1d08e1579c046ea71c2ccfa1ff8a73b868496b12
Gonxped31 <115489183+Gonxped31@users.noreply.github.com>
2023-06-03 22:19:45 -0400

    Add files via upload

---
+++ robotix/src/domain/logic/Fournisseur/Fournisseur.java
@@ -0,0 +1,49 @@
+package domain.logic.Fournisseur;
+
+import domain.logic.Robot.Robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fournisseur{
+    public String nom;
+    public String adresse;
+    public String email;
+    public String numeroTelephone;
+    public String typeRobotFabriquer;
+    public String capacite;
+    public String nomCompagnie;
+    public String pseudo;
+    public List<Robot> inventaireDeRobot=new ArrayList<>();
+    public List<String> inventaireComposant= new ArrayList<>();
+
+    public Fournisseur(String nom, String adresse, String pseudo, String email, String numeroTelephone,
+                       String typeDeRobotFabriquer, String capacite ,String nomcompagnie){
+        this.nom=nom;
+        this.adresse=adresse;
+        this.pseudo=pseudo;
+        this.email=email;
+        this.numeroTelephone=numeroTelephone;
+        this.typeRobotFabriquer=typeDeRobotFabriquer;
+        this.capacite=capacite;
+        this.nomCompagnie=nomcompagnie;
+    }
+    public void vendreUnComposant(String composant){
+      inventaireComposant.remove(composant);
+    }
+    public void mettreInventaireRobotAjour(Robot robot, boolean modeAjout){
+        if (modeAjout) {
+            inventaireDeRobot.add(robot);
+        } else {
+            inventaireDeRobot.remove(robot);
+        }
+    }
+
+    public void mettreInventaireComposantAjour( String composant, boolean modeAjout){
+        if (modeAjout) {
+            inventaireComposant.add(composant);
+        } else {
+            inventaireComposant.remove(composant);
+        }
+    }
+}
---
+++ robotix/src/domain/logic/Robot/Robot.java
@@ -0,0 +1,28 @@
+package domain.logic.Robot;
+import java.util.LinkedList;
+import java.util.UUID;
+// Pourquoi est-ce qu'un robot doit prendre des actions et des tâches en paramètre ?
+public class Robot {
+    public String nom;
+    public int X, Y, vitesse, batterie, cpu;
+    public double memoire; // En GB
+    public LinkedList<String> composantes = new LinkedList<String>();
+    public LinkedList<String> actions = new LinkedList<String>(); 
+    public LinkedList<String> taches = new LinkedList<String>(); 
+    public LinkedList<String> activites = new LinkedList<String>();
+    public String numeroSerie;
+    public Robot(String nom,int X, int Y, int vitesse, int batterie, int cpu, double memoire,LinkedList<String> composantes, LinkedList<String> action, LinkedList<String> taches, LinkedList<String> activites, String numeroSerie){
+        this.nom = nom;
+        this.X = X;
+        this.Y = Y;
+        this.vitesse = vitesse;
+        this.batterie = batterie;
+        this.cpu = cpu;
+        this.memoire = memoire;
+        this.composantes = composantes;
+        this.actions = action;
+        this.taches = taches;
+        this.activites = activites;
+        this.numeroSerie = numeroSerie;
+    }
+}
