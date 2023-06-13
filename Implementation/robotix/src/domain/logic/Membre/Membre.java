59b6acea063c9befbfbd734408b57b724f59e5f8
gonxped31 <gonxped31@gmail.com>
2023-06-12 21:11:36 -0400

    added folders

---
+++ Implementation/robotix/src/domain/logic/Membre/Membre.java
@@ -0,0 +1,21 @@
+package domain.logic;
+public class Membre {
+    public String nom;
+    public String adresse;
+    public String email;
+    public String numeroTelephone;
+    public String nomCompagnie;
+
+    public Membre(String nom, String adresse, String email, String numeroTelephone, String nomCompagnie) {
+        this.nom = nom;
+        this.adresse = adresse;
+        this.email = email;
+        this.numeroTelephone = numeroTelephone;
+        this.nomCompagnie = nomCompagnie;
+    }
+    
+    public static void name() {
+        
+    }
+
+}
