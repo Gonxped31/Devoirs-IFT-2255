9005d5c1ec4d3ddff34b5f9c4a129b13ac5931e6
Dorensky <64221942+Dorensky@users.noreply.github.com>
2023-06-28 11:39:24 -0400

    Implémentation des méthodes dans MenuFournisseur

--- Implementation/robotix/src/domain/logic/Menu/MenuFournisseur.java
+++ Implementation/robotix/src/domain/logic/Menu/MenuFournisseur.java
@@ -1,5 +1,37 @@
 package domain.logic.Menu;
 
 public class MenuFournisseur {
-    
+
+    public void menuInscriptionFournisseur() {
+
+    }
+
+    public void menuConnexionFournisseur() {
+
+    }
+
+    public void menuFournisseur() {
+
+    }
+
+    public void menuModifierProfileFournisseur() {
+
+    }
+
+    public void menuGererComposantes() {
+
+    }
+
+    public void menuEneregistrerComposante() {
+
+    }
+
+    public void menuNotifications() {
+
+    }
+
+    public void menuVoirProfileFournisseur() {
+
+    }
+
 }
