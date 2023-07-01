65d07bf1f1a90cc53a21ff793ea2c75cdab9fbf2
gonxped31 <gonxped31@gmail.com>
2023-07-01 13:19:26 -0400

    checking

--- Implementation/robotix/src/service/BaseDeDonneeFournisseur.java
+++ Implementation/robotix/src/service/BaseDeDonneeFournisseur.java
@@ -31,20 +31,23 @@ public class BaseDeDonneeFournisseur  extends BaseDeDonneeCommun{
          //Todo
         List<Fournisseur> tempList= new ArrayList<>(Arrays.asList(
 
-       /*new Fournisseur("Roy", "123 rue des Innovations, Montr�al, QC, H1A 0A1",  "nom1@robotech.ca",
-                        "5142104555", "RobotA", "CPU", "30", "RoboTechnologies")
-
-        new Fournisseur("Roy", "123 rue des Innovations, Montr�al, QC, H1A 0A1", "roy1", "nom1@robotech.ca",
+       new Fournisseur("Roy", "123" ,"123 rue des Innovations, Montr�al, QC, H1A 0A1",  "nom1@robotech.ca",
                         "5142104555", "RobotA", "CPU", "30", "RoboTechnologies"),
 
-        new Fournisseur("Bouchard", "456 avenue des Automates, Montr�al, QC, H5M 1N2", "bouchard2",
+        new Fournisseur("Roy", "456", "123 rue des Innovations, Montr�al, QC, H1A 0A1", "nom1@robotech.ca",
+                        "5142104555", "RobotA", "CPU", "30", "RoboTechnologies"),
+
+        new Fournisseur("Bouchard", "789","456 avenue des Automates, Montr�al, QC, H5M 1N2",
                         "contact@automatech.ca", "4503335432", "RobotB", "BRAS", "25", "Automatech"),
-        new Fournisseur("Adams", "2376 boulevard des G�nies, Qu�bec, QC, G1W 2W5", "adams3",
+
+        new Fournisseur("Adams", "159","2376 boulevard des G�nies, Qu�bec, QC, G1W 2W5",
                         "service@innovatech.ca", "4509998888", "RobotC", "ECRAN","27", "Innovatech"),
-        new Fournisseur("Wilson", "89 boulevard de la Technologie, Laval, QC, H7M 7B7", "wilson4",
+
+        new Fournisseur("Wilson", "753","89 boulevard de la Technologie, Laval, QC, H7M 7B7",
                         "assistance@iRobot.ca", "4502109876", "RobotD", "CAMERA","35", "iRobot"),
-        new Fournisseur("Thompson", "10 Place de la Robotique, Longueuil, QC, J4H 1A1", "thompson5",
-                        "info@roboPro.ca", "4506780000", "RobotE", "HAUTPARLEUR","22", "RoboPro") */
+
+        new Fournisseur("Thompson", "486", "10 Place de la Robotique, Longueuil, QC, J4H 1A1",
+                        "info@roboPro.ca", "4506780000", "RobotE", "HAUTPARLEUR","22", "RoboPro")
         ));
 
         tempList.stream().forEach(fournisseur -> {
