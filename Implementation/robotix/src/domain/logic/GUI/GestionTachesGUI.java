f8e3d8728bf591783d0ced73fdd4eb2f8c5e0208
Dorensky <64221942+Dorensky@users.noreply.github.com>
2023-07-18 00:11:58 -0400

    Create GestionTachesGUI.java

---
+++ Implementation/robotix/src/domain/logic/GUI/GestionTachesGUI.java
@@ -0,0 +1,20 @@
+package domain.logic.GUI;
+
+import javax.swing.*;
+
+public class GestionTachesGUI {
+    private JPanel gestionTachesPanel = new JPanel();
+    private JLabel gestionTachesLabel = new JLabel("Gestion de mes taches");
+    private JButton btnCreerTache = new JButton("Creer une tache");
+    private JButton btnAllouerTache = new JButton("Allouer une tache a un robot");
+
+    public GestionTachesGUI() {
+        gestionTachesPanel.add(gestionTachesLabel);
+        gestionTachesPanel.add(btnCreerTache);
+        gestionTachesPanel.add(btnAllouerTache);
+    }
+
+    public JPanel getPanel() {
+        return gestionTachesPanel;
+    }
+}
