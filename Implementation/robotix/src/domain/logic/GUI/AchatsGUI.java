11dcf99e2d3b39868739e4ce60336e3920312d4b
Dorensky <64221942+Dorensky@users.noreply.github.com>
2023-07-19 00:24:05 -0400

    Create AchatsGUI.java

---
+++ Implementation/robotix/src/domain/logic/GUI/AchatsGUI.java
@@ -0,0 +1,20 @@
+package domain.logic.GUI;
+
+import javax.swing.*;
+
+public class AchatsGUI {
+    private JPanel achatsPanel = new JPanel();
+    private JLabel achatsLabel = new JLabel("Que voulez-vous acheter?");
+    private JButton btnAchatRobot = new JButton("Robot");
+    private JButton btnAchatComposante = new JButton("Composante");
+
+    public AchatsGUI() {
+        achatsPanel.add(achatsLabel);
+        achatsPanel.add(btnAchatRobot);
+        achatsPanel.add(btnAchatComposante);
+    }
+
+    public JPanel getPanel() {
+        return achatsPanel;
+    }
+}
