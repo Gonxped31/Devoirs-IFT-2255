5fb1d06edaabe40b43e89f36493d2169dfb93f4e
Dorensky <64221942+Dorensky@users.noreply.github.com>
2023-07-19 00:17:28 -0400

    Create GestionReseauGUI.java

---
+++ Implementation/robotix/src/domain/logic/GUI/GestionReseauGUI.java
@@ -0,0 +1,21 @@
+package domain.logic.GUI;
+
+import javax.swing.*;
+
+public class GestionReseauGUI {
+    private JPanel gestionReseauPanel = new JPanel();
+    private JLabel gestionReseauLabel = new JLabel("Gestion reseau");
+    private JButton btnSuivreUtilisateur = new JButton("Suivre un utilisateur");
+    private JButton btnGererSuiveurs = new JButton("Gerer mes suiveurs");
+    private JButton btnGererInterets = new JButton("Gerer mes interets");
+
+    public GestionReseauGUI() {
+        gestionReseauPanel.add(gestionReseauLabel);
+        gestionReseauPanel.add(btnSuivreUtilisateur);
+        gestionReseauPanel.add(btnGererSuiveurs);
+    }
+
+    public JPanel getPanel() {
+        return gestionReseauPanel;
+    }
+}
