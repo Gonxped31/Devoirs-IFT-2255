36eb64b48b28a2dbfb4c2dbecdd42dc9a107f945
Dorensky <64221942+Dorensky@users.noreply.github.com>
2023-07-21 11:56:58 -0400

    Update MenusFournisseur.java

--- Implementation/robotix/src/domain/logic/Menu/MenusFournisseur.java
+++ Implementation/robotix/src/domain/logic/Menu/MenusFournisseur.java
@@ -26,13 +26,36 @@ public class MenusFournisseur extends JFrame {
 	private JLabel menuFournisseurLabel = new JLabel("Menu Fournisseur");
 	private JLabel inscriptionFournisseurLabel = new JLabel("Inscription en tant que fournisseur");
 	private JLabel connexionFournisseurLabel = new JLabel("Connexion en tant que fournisseur");
+	// INSCRIPTION ET CONNEXION
+	private JLabel nomLabel = new JLabel("Nom (ce nom sera celui affiche lors de la visite de votre profil par un utilisateur)");
+	private JLabel courrielLabel = new JLabel("Adresse courriel");
+	private JLabel mdpLabel = new JLabel("Mot de passe");
+	private JLabel confirmerMdpLabel = new JLabel("Confirmer le mot de passe");
+	private JLabel telephoneLabel = new JLabel("Numero de telephone");
+	private JLabel adresseLabel = new JLabel("Adresse");
+	private JLabel typeRobotFabriquesLabel = new JLabel("Type de robots fabriques");
+	private JLabel typeComposantesFabriquesLabel = new JLabel("Type de composantes fabriques");
+	private JLabel capaciteFabricationLabel = new JLabel("Capacite de fabrication");
+	private JLabel nomCompagnieLabel = new JLabel("Nom de compagnie");
+	private JTextField nomField = new JTextField();
+	private JTextField courrielField = new JTextField();
+	private JPasswordField mdpField = new JPasswordField();
+	private JPasswordField confirmerMdpField = new JPasswordField();
+	private JTextField telephoneField = new JTextField();
+	private JTextField adresseField = new JTextField();
+	private JTextField typeRobotFabriquesField = new JTextField();
+	private JTextField typeComposantesFabriquesField = new JTextField();
+	private JTextField capaciteFabricationField = new JTextField();
+	private JTextField nomCompagnieField = new JTextField();
+	private JButton btnSeConnecter = new JButton("Se Connecter");
+	private JButton btnConfirmerInscription = new JButton("Confirmer");
+	private JButton btnRetour = new JButton("Retour");
 	private JButton btnAjouterRobot = new JButton("Ajouter un nouveau robot");
 	private JButton btnRetirerRobot = new JButton("Retirer un robot");
 	private JButton btnEnregistrerComposante = new JButton("Enregistrer une composante");
 	private JButton btnGererComposante = new JButton("Gerer mes composantes");
 	private JButton btnModifierProfil = new JButton("Modifier mon profil");
 	private JButton btnRequetePublique = new JButton("Faire une requete publique");
-	private JButton btnRetour = new JButton("Retour");
 	private AjouterRobotGUI ajouterRobotGUI = new AjouterRobotGUI();
 	private RetirerRobotGUI retirerRobotGUI = new RetirerRobotGUI();
 	private EnregistrerComposanteGUI enregistrerComposanteGUI = new EnregistrerComposanteGUI();
@@ -136,10 +159,62 @@ public class MenusFournisseur extends JFrame {
 		menuFournisseurPanel.add(btnRetour);
 	}
 	public void afficherFormulaireInscription(JFrame jFrame) {
+		this.jFrame = jFrame;
+		creerFormulaireInscription();
+		this.jFrame.getContentPane().removeAll();
+		this.jFrame.setContentPane(inscriptionFournisseurPanel);
+		this.jFrame.revalidate();
+		this.jFrame.repaint();
+	}
+
+	public void creerFormulaireInscription() {
+		inscriptionFournisseurPanel.setLayout(new GridLayout(0, 2, 5, 5));
+		inscriptionFournisseurPanel.add(inscriptionFournisseurLabel);
+		inscriptionFournisseurPanel.add(Box.createHorizontalStrut(10));
+		inscriptionFournisseurPanel.add(nomLabel);
+		inscriptionFournisseurPanel.add(nomField);
+		inscriptionFournisseurPanel.add(courrielLabel);
+		inscriptionFournisseurPanel.add(courrielField);
+		inscriptionFournisseurPanel.add(mdpLabel);
+		inscriptionFournisseurPanel.add(mdpField);
+		inscriptionFournisseurPanel.add(confirmerMdpLabel);
+		inscriptionFournisseurPanel.add(confirmerMdpField);
+		inscriptionFournisseurPanel.add(telephoneLabel);
+		inscriptionFournisseurPanel.add(telephoneField);
+		inscriptionFournisseurPanel.add(adresseLabel);
+		inscriptionFournisseurPanel.add(adresseField);
+		inscriptionFournisseurPanel.add(typeRobotFabriquesLabel);
+		inscriptionFournisseurPanel.add(typeRobotFabriquesField);
+		inscriptionFournisseurPanel.add(typeComposantesFabriquesLabel);
+		inscriptionFournisseurPanel.add(typeComposantesFabriquesField);
+		inscriptionFournisseurPanel.add(capaciteFabricationLabel);
+		inscriptionFournisseurPanel.add(capaciteFabricationField);
+		inscriptionFournisseurPanel.add(nomCompagnieLabel);
+		inscriptionFournisseurPanel.add(nomCompagnieField);
+		inscriptionFournisseurPanel.add(Box.createHorizontalStrut(10));
+		inscriptionFournisseurPanel.add(btnConfirmerInscription);
+		inscriptionFournisseurPanel.add(btnRetour);
 	}
 
 	public void afficherFormulaireConnexion(JFrame jFrame) {
+		this.jFrame = jFrame;
+		creerFormulaireConnexion();
+		this.jFrame.getContentPane().removeAll();
+		this.jFrame.setContentPane(connexionFournisseurPanel);
+		this.jFrame.revalidate();
+		this.jFrame.repaint();
+	}
 
+	public void creerFormulaireConnexion() {
+		connexionFournisseurPanel.setLayout(new GridLayout(0, 2, 5, 5));
+		connexionFournisseurPanel.add(connexionFournisseurLabel);
+		connexionFournisseurPanel.add(Box.createHorizontalStrut(10));
+		connexionFournisseurPanel.add(nomLabel);
+		connexionFournisseurPanel.add(nomField);
+		connexionFournisseurPanel.add(mdpLabel);
+		connexionFournisseurPanel.add(mdpField);
+		connexionFournisseurPanel.add(btnSeConnecter);
+		connexionFournisseurPanel.add(btnRetour);
 	}
 
 	public void afficherEnregistrerComposante() {
