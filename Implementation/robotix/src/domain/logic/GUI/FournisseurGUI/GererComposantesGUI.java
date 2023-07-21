d578e1393122bbf6fa144f91179cf33854dc8adb
Dorensky <64221942+Dorensky@users.noreply.github.com>
2023-07-21 11:29:45 -0400

    Update GererComposantesGUI

--- Implementation/robotix/src/domain/logic/GUI/FournisseurGUI/GererComposantesGUI.java
+++ Implementation/robotix/src/domain/logic/GUI/FournisseurGUI/GererComposantesGUI.java
@@ -4,9 +4,16 @@ import javax.swing.*;
 
 public class GererComposantesGUI {
     private JPanel gererComposantesPanel = new JPanel();
+    private JLabel gererComposantesLabel = new JLabel("Choisissez une option");
+    private JButton btnSupprimerComposante = new JButton();
+    private JButton btnModifierPrixComposante = new JButton();
+    private JButton btnModifierDescComposante = new JButton();
 
     public GererComposantesGUI() {
-
+        gererComposantesPanel.add(gererComposantesLabel);
+        gererComposantesPanel.add(btnSupprimerComposante);
+        gererComposantesPanel.add(btnModifierPrixComposante);
+        gererComposantesPanel.add(btnModifierDescComposante);
     }
 
     public JPanel getPanel() {
