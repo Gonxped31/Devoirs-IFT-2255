fdbe41ef69b8ae128f70c0d1cb042f861510b40f
Dorensky <64221942+Dorensky@users.noreply.github.com>
2023-07-25 21:18:30 -0400

    Update GererComposanteGUI

--- Implementation/robotix/src/domain/logic/GUI/FournisseurGUI/GererComposantesGUI.java
+++ Implementation/robotix/src/domain/logic/GUI/FournisseurGUI/GererComposantesGUI.java
@@ -20,9 +20,9 @@ public class GererComposantesGUI {
     private JLabel modifierPrixComposanteLabel = new JLabel("Modifier le prix d'une composante");
     private JLabel modifierNomComposanteLabel = new JLabel("Nom de la composante a modifier");
     private JLabel modifierDescComposanteLabel = new JLabel("Modifier la description d'une composante");
-    private JTextField nomComposante = new JTextField();
-    private JTextField prixComposante = new JTextField();
-    private JTextField descComposante = new JTextField();
+    private JTextField nomComposanteField = new JTextField();
+    private JTextField prixComposanteField = new JTextField();
+    private JTextField descComposanteField = new JTextField();
     private JButton btnSupprimer = new JButton("Supprimer");
     private JButton btnModifierPrix = new JButton("Modifier le prix");
     private JButton btnModifierDesc = new JButton("Modifier la description");
@@ -71,8 +71,9 @@ public class GererComposantesGUI {
     public void setSupprimerComposantePanel() {
         supprimerComposantePanel.setLayout(new GridLayout(0, 2, 5, 5));
         supprimerComposantePanel.add(supprimerComposanteLabel);
+        supprimerComposantePanel.add(Box.createHorizontalStrut(10));
         supprimerComposantePanel.add(supprimerNomComposanteLabel);
-        supprimerComposantePanel.add(nomComposante);
+        supprimerComposantePanel.add(nomComposanteField);
         supprimerComposantePanel.add(btnConfirmerSupprression);
         supprimerComposantePanel.add(btnAnnuler);
     }
@@ -80,9 +81,10 @@ public class GererComposantesGUI {
     public void setModifierPrixComposantePanel() {
         modifierPrixComposantePanel.setLayout(new GridLayout(0, 2, 5, 5));
         modifierPrixComposantePanel.add(modifierPrixComposanteLabel);
+        modifierPrixComposantePanel.add(Box.createHorizontalStrut(10));
         modifierPrixComposantePanel.add(modifierNomComposanteLabel);
-        modifierPrixComposantePanel.add(nomComposante);
-        modifierPrixComposantePanel.add(prixComposante);
+        modifierPrixComposantePanel.add(nomComposanteField);
+        modifierPrixComposantePanel.add(prixComposanteField);
         modifierPrixComposantePanel.add(btnConfirmerModifPrix);
         modifierPrixComposantePanel.add(btnAnnuler);
     }
@@ -90,8 +92,9 @@ public class GererComposantesGUI {
     public void setModifierDescComposantePanel() {
         modifierDescComposantePanel.setLayout(new GridLayout(0, 2, 5, 5));
         modifierDescComposantePanel.add(modifierDescComposanteLabel);
+        modifierDescComposantePanel.add(Box.createHorizontalStrut(10));
         modifierDescComposantePanel.add(modifierNomComposanteLabel);
-        modifierDescComposantePanel.add(descComposante);
+        modifierDescComposantePanel.add(descComposanteField);
         modifierDescComposantePanel.add(btnConfirmerModifDesc);
         modifierDescComposantePanel.add(btnAnnuler);
     }
